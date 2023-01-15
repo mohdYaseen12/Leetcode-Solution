d72dbb303947527d492fea05a0ae1142e45d5e29
md-yaseeny <95025453+md-yaseeny@users.noreply.github.com>
2023-01-15 19:11:14 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (57.17%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    private int maxIdx(int[] nums){
+        int start = 0;
+        int end = nums.length-1;
+
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(mid+1 < nums.length && nums[mid] > nums[mid+1] ) return mid;
+            else if(nums[start] <= nums[mid]) start = mid+1;
+            else end = mid - 1;
+        }
+        return nums.length-1;
+    }
+
+    private int binarySearch(int[] nums, int target, int start, int end){
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] <= target) start = mid + 1;
+            else end = mid - 1;
+        }
+        return -1;
+    }
+    public int search(int[] nums, int target) {
+        int idx = maxIdx(nums);
+        if(nums[idx] == target) return idx;
+        else if(nums[0] <= target && nums[idx] > target) return binarySearch(nums,target,0,idx);
+        else return binarySearch(nums,target,idx+1,nums.length-1);
+    }
+}
