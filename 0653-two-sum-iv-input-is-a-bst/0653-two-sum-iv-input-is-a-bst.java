2c556c67f0e334319192ff70a3f63f3401a7e29d
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-01 14:49:20 +0530

    Time: 3 ms (89.72%), Space: 42.4 MB (97.07%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,23 +14,37 @@
  * }
  */
 class Solution {
-    private void inorder(TreeNode root, ArrayList<Integer> in){
-        if(root == null) return;
-        inorder(root.left,in);
-        in.add(root.val);
-        inorder(root.right,in);
+    // private void inorder(TreeNode root, ArrayList<Integer> in){
+    //     if(root == null) return;
+    //     inorder(root.left,in);
+    //     in.add(root.val);
+    //     inorder(root.right,in);
+    // }
+    // public boolean findTarget(TreeNode root, int k) {
+    //     ArrayList<Integer> in = new ArrayList<>();
+    //     inorder(root,in);
+    //     int i=0;
+    //     int j = in.size()-1;
+    //     while(i < j){
+    //         int sum = in.get(i) + in.get(j);
+    //         if(sum == k) return true;
+    //         else if(sum < k) i++;
+    //         else j--;
+    //     }
+    //     return false;
+    // }
+    
+     public boolean findTarget(TreeNode root, int k){
+         HashSet<Integer> set = new HashSet<>();
+         return dfs(root,k,set);
+     }
+    
+    private boolean dfs(TreeNode root, int k, HashSet<Integer> set){
+        if(root == null) return false;
+        if(set.contains(k - root.val)) return true;
+        set.add(root.val);
+        
+        return dfs(root.left,k,set) || dfs(root.right,k,set);
     }
-    public boolean findTarget(TreeNode root, int k) {
-        ArrayList<Integer> in = new ArrayList<>();
-        inorder(root,in);
-        int i=0;
-        int j = in.size()-1;
-        while(i < j){
-            int sum = in.get(i) + in.get(j);
-            if(sum == k) return true;
-            else if(sum < k) i++;
-            else j--;
-        }
-        return false;
-    }
+    
 }
