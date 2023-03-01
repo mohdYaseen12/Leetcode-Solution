015d0060cf73ad8e440a7a02b1ef4a8b19a33794
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-01 15:38:22 +0530

    Time: 6 ms (17.20%), Space: 43.3 MB (37.63%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,37 +1,69 @@
 class Solution {
-    public void dfs(List<List<Integer>> graph,int src,boolean[] visited){
-        visited[src] = true;
-       for(int nbr : graph.get(src)){
-           if(!visited[nbr]){
-               dfs(graph,nbr,visited);
-           }
-       }
+    // ************** DFS Solution ****************
+    
+//     public void dfs(List<List<Integer>> graph,int src,boolean[] visited){
+//         visited[src] = true;
+//        for(int nbr : graph.get(src)){
+//            if(!visited[nbr]){
+//                dfs(graph,nbr,visited);
+//            }
+//        }
         
-    }
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0;i<n;i++) graph.add(new ArrayList<>());
+//     }
+//     public int findCircleNum(int[][] isConnected) {
+//         int n = isConnected.length;
+//         List<List<Integer>> graph = new ArrayList<>();
+//         for(int i=0;i<n;i++) graph.add(new ArrayList<>());
         
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i == j) continue;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i == j) continue;
                 
-                if(isConnected[i][j] == 1) graph.get(i).add(j);
-            }
-        }
+//                 if(isConnected[i][j] == 1) graph.get(i).add(j);
+//             }
+//         }
         
+        
+//         boolean[] visited = new boolean[n];
+//         int count = 0;
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 count++;
+//                 dfs(graph,i,visited);
+//             }
+//         }
+//         return count;
+        
+//     }
+    
+    
+    
+    // ****************  BFS SOLUTION ***************
+    public int findCircleNum(int[][] isConnected){
+        
+        int n = isConnected.length;
+        int provinces = 0;
         
         boolean[] visited = new boolean[n];
-        int count = 0;
+        Queue<Integer> q = new ArrayDeque<>();
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                count++;
-                dfs(graph,i,visited);
+                q.add(i);
+                provinces++;
+            }
+            
+            while(q.size() > 0){
+                int curr = q.remove();
+                visited[curr] = true;
+                
+                for(int j=0; j< n; j++){
+                    if( j != curr && !visited[j] && isConnected[curr][j] == 1){
+                        q.add(j);
+                    }
+                }
             }
         }
-        return count;
-        
+        return provinces;
     }
 }
