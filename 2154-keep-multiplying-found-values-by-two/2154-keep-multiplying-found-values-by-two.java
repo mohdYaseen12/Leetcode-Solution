6f73e262a9452b7eebf22ba273501547288667d9
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-12 23:15:58 +0530

    Time: 3 ms (48.82%), Space: 42.2 MB (29.81%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.java
@@ -1,18 +1,30 @@
 class Solution {
     public int findFinalValue(int[] nums, int original) {
-        Set<Integer> set = new HashSet<>();
+        // APPROACH USING HASHSET
         
+        
+//         Set<Integer> set = new HashSet<>();
+        
+//         for(int num : nums){
+//             // add num into set
+//              set.add(num);
+//         }
+        
+//         for(int num : set){
+//              // check if set has any element which is equal to original
+//             if(set.contains(original)){
+//                 // update original
+//                 original *= 2;   
+//             }
+//         }
+//         return original;
+        
+        // APPROACH 2: USING SORTING AND NORMAL ITERATION THROUGH LOOP IN GIVEN ARRAY NUMS
+        
+        
+        Arrays.sort(nums);
         for(int num : nums){
-            // add num into set
-             set.add(num);
-        }
-        
-        for(int num : set){
-             // check if set has any element which is equal to original
-            if(set.contains(original)){
-                // update original
-                original *= 2;   
-            }
+            if(num == original) original = original * 2;
         }
         return original;
     }
