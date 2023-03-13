0e8a2212e8482b8645c8d95722643cd6f610bb32
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-13 18:15:34 +0530

    Time: 62 ms (84.42%), Space: 57.1 MB (92.10%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,23 +1,45 @@
+// ***************** APPROACH 2: Using Queue ****************
 class Solution {
     public int[] findOriginalArray(int[] changed) {
-        if(changed.length == 1 || changed.length % 2 != 0) return new int[0];
+        if(changed.length == 1 || changed.length % 2 == 1) return new int[0];
+        Queue<Integer> q = new LinkedList<>();
         
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-        
-        for(int val : changed) map.put(val,map.getOrDefault(val,0)+1);
-        
+        Arrays.sort(changed);
         int[] original = new int[changed.length / 2];
         int idx = 0;
         
-        for(int key : map.keySet()){
-            if(map.get(key) > map.getOrDefault(key + key, 0)) return new int[0];
-            
-            for(int j=0;j<map.get(key);j++){
-                original[idx++] = key;
-                
-                map.put(key+key,map.getOrDefault(key+key,0)-1);
-            }
+        for(int num : changed){
+            if(!q.isEmpty() && q.peek() == num)
+                original[idx++] = q.remove()/2;
+            else q.add(num * 2);
         }
-        return original;
+         return q.size() > 0 ? new int[0] : original;
     }
 }
+
+
+// ************** APPRAOCH 1: Using TreeMap **************
+// class Solution {
+//     public int[] findOriginalArray(int[] changed) {
+//         if(changed.length == 1 || changed.length % 2 != 0) return new int[0];
+        
+//         TreeMap<Integer, Integer> map = new TreeMap<>();
+        
+//         for(int val : changed) map.put(val,map.getOrDefault(val,0)+1);
+        
+//         int[] original = new int[changed.length / 2];
+//         int idx = 0;
+        
+//         for(int key : map.keySet()){
+//             if(map.get(key) > map.getOrDefault(key + key, 0)) return new int[0];
+            
+//             for(int j=0;j<map.get(key);j++){
+//                 original[idx++] = key;
+                
+//                 map.put(key+key,map.getOrDefault(key+key,0)-1);
+//             }
+//         }
+//         return original;
+//     }
+// }
+
