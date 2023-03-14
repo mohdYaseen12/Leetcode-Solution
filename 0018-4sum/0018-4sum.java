f8104f472e50040e6373a42c73ee4c6d708638d5
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-14 23:45:38 +0530

    Time: 31 ms (37.90%), Space: 43 MB (42.94%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,42 +1,55 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-
-       List<List<Integer>> fr = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-        if(target == -294967296) {
-
-            return fr;
-        }
-
         Arrays.sort(nums);
-        for(int i = 0;i< nums.length;i++) {
-            for (int j = i+1;j< nums.length;j++) {
-                int k = j+1;
-                int end = nums.length-1;
-                while(k < nums.length && end > 0 && end !=k && end >k) {
-                   int sum = nums[i] + nums[j] + nums[k] + nums[end];
-                    if(sum == target) {
-                     result.add(nums[i]);
-                         result.add(nums[j]);
-                     result.add(nums[k]);
-                     result.add(nums[end]);
-                  if(result.size() ==4 ) {
-                     Collections.sort(result);
-                     if(!fr.contains(result)) {
-                     fr.add(result);
-                     }
-                  }
-                     k++;
-                     end--;
-                     result = new ArrayList<>();
-                    } else if(sum < target) {
-                        k++;
-                    } else {
-                        end--;
-                    }   
-                }       
-            }
+        List<List<Integer>> ans = new ArrayList<>();
+        if(target == -294967296 || target == 294967296){
+            return ans;
         }
-        return fr;
+        // fixing first element and then searching for next three element
+        for(int i=0;i<nums.length;i++){
+            // fixing second element and searching for next two element
+            for(int j=i+1;j<nums.length;j++){
+               
+                // normal two Sum solution
+                int newTar = target - (nums[i] + nums[j]); // getting new target for two sum
+                int start = j+1; // fixing start index as next index from the second fixed element 
+                int end = nums.length-1; // fixind end pointer --> pointing to the last element
+                
+                while(start < end){
+                    int sum = nums[start] + nums[end];
+                    if(sum == newTar){ // found the all four required element
+                        List<Integer> curr = new ArrayList<>();
+                        curr.add(nums[i]);
+                        curr.add(nums[j]);
+                        curr.add(nums[start]);
+                        curr.add(nums[end]);
+                        ans.add(curr);
+                        start++;
+                        // moving next for same elements.
+                        while(start < end && nums[start] == nums[start-1]) start++;
+                        
+                        end--;
+                        // moving back from end for the same element
+                        while(end > start && nums[end] == nums[end+1]) end--;
+                    }
+                    else if(sum < newTar){
+                        start++;
+                        // checking for same element and moving forward
+                        while(start < end && nums[start] == nums[start-1]) start++;
+                    }
+                    else{
+                        end--;
+                        // checking for prev same element and moving backward 
+                        while(end > start && nums[end] == nums[end+1]) end--;
+                    }
+                }
+                // checking and moving forward for same element
+                while(j+1 < nums.length && nums[j] == nums[j+1]) j++;
+            }
+            // checking and moving forward for same element
+            while(i+1 < nums.length && nums[i] == nums[i+1]) i++;
+        }
+        
+        return ans;
     }
 }
