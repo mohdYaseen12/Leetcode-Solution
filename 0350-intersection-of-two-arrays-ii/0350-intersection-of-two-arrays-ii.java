ab36c57f846a8f27aae0092e893cb8ec62ccdcea
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-17 15:39:49 +0530

    Time: 3 ms (52.78%), Space: 42.5 MB (62.88%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,25 +1,45 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+//         Arrays.sort(nums1);
+//         Arrays.sort(nums2);
         
-        int i = 0;
-        int j = 0;
-        List<Integer> ans = new ArrayList<>();
+//         int i = 0;
+//         int j = 0;
+//         List<Integer> ans = new ArrayList<>();
         
-        while(i < nums1.length && j < nums2.length){
+//         while(i < nums1.length && j < nums2.length){
             
-            if(nums1[i] == nums2[j]){
-                ans.add(nums1[i]);
-                i++;
-                j++;
-            }
-            else if(nums1[i] < nums2[j]){
-                i++;
-            }
-            else j++;
+//             if(nums1[i] == nums2[j]){
+//                 ans.add(nums1[i]);
+//                 i++;
+//                 j++;
+//             }
+//             else if(nums1[i] < nums2[j]){
+//                 i++;
+//             }
+//             else j++;
+//         }
+        
+//         int[] res = new int[ans.size()];
+//         for(int idx = 0; idx < ans.size();idx++){
+//             res[idx] = ans.get(idx);
+//         }
+//         return res;
+        
+        // ************** USING HASHMAP *************************//
+        Map<Integer,Integer> hm = new HashMap<>();
+        for(int num : nums1){
+            hm.put(num,hm.getOrDefault(num,0)+1);
         }
         
+        List<Integer> ans = new ArrayList<>();
+        
+        for(int num : nums2){
+            if(hm.containsKey(num) && hm.get(num) > 0){
+                ans.add(num);
+                hm.put(num,hm.getOrDefault(num,0)-1);
+            }
+        }
         int[] res = new int[ans.size()];
         for(int idx = 0; idx < ans.size();idx++){
             res[idx] = ans.get(idx);
