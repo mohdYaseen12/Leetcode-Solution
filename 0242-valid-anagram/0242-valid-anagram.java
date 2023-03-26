efd3ea405bbea6f78955a0a8626da9cf6e9f92c8
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-26 16:12:50 +0530

    Time: 17 ms (21.14%), Space: 43.7 MB (10.32%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,33 +1,6 @@
 class Solution {
     public boolean isAnagram(String str1, String str2) {
-        // char ch[] = new char[str1.length()];
-        // char ch1[] = new char[str2.length()];
-
-        // for(int i=0;i<str1.length();i++){
-        //     ch[i] = str1.charAt(i);
-        // }
-        // for(int i=0;i<str2.length();i++){
-        //     ch1[i] = str2.charAt(i);
-        // }
-        // Arrays.sort(ch);
-        // Arrays.sort(ch1);
-        // boolean flag = true;
-        // if(str1.length() != str2.length()){
-        //     return false;
-        // }
-        // else{
-        //     for(int i=0; i<ch.length;i++){
-        //         if(ch[i] != ch1[i]){
-        //             flag = false;
-        //             break;
-        //         }
-        //     }
-        // }
-        // return flag;
-
-
-        // using hashMap
-
+        
         if(str1.length() != str2.length()) return false;
 	   
 	   HashMap<Character,Integer> hm = new HashMap<>();
@@ -41,6 +14,5 @@ class Solution {
 		   if(freq != 0) return false;
 	   }
 	   return true;
-	   
     }
 }
