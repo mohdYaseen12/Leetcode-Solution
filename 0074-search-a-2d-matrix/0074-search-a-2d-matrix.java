86eed87d958951bd679d5de708d5ebd4924d5088
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-03-27 14:50:07 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (5.76%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,36 +1,18 @@
 class Solution {
-    private int floor(int[][] matrix, int target){
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = matrix.length;
+        int cols = matrix[0].length;
+
         int start = 0;
-        int end = matrix.length-1;
-        int ans = -1;
+        int end = (row * cols)-1;
         while(start <= end){
             int mid = (start + end) / 2;
-            if(matrix[mid][0] == target) return mid;
-            else if(target > matrix[mid][0]){
-                ans = mid;
-                start = mid+1;
-            }
-            else end = mid-1;
-        }
-        return ans;
-    }
-
-    private boolean binarySearch(int[][] matrix, int target, int row){
-        int start = 0;
-        int end = matrix[row].length-1;
-
-        while(start <= end){
-            int mid = (start + end) / 2;
-            if(matrix[row][mid] == target) return true;
-            else if(matrix[row][mid] < target) start = mid+1;
+            int r = mid / cols;
+            int c = mid % cols;
+            if(matrix[r][c] == target) return true;
+            else if(matrix[r][c] < target) start = mid+1;
             else end = mid-1;
         }
         return false;
-    }
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int row = floor(matrix,target);
-        if(row == -1) return false;
-
-        return binarySearch(matrix,target,row);
     }
 }
