b3552af97a70b1bdb9f54cc0b48d0be6183238c0
Md Yaseen <95025453+md-yaseeny@users.noreply.github.com>
2023-04-27 18:04:24 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (53.72%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.java
+++ 0319-bulb-switcher/0319-bulb-switcher.java
@@ -1,5 +1,18 @@
 class Solution {
     public int bulbSwitch(int n) {
-        return (int)Math.sqrt(n);
+        // return (int)Math.sqrt(n);  Approach 1
+        
+        // Approach 2 -->> Using Binary Search
+        
+        long low = 0;
+        long high = n;
+        while(low <= high){
+            long mid = low + (high - low) / 2;
+            if((mid*mid) == n) return (int)mid;
+            else if((mid*mid) < n) low = mid+1;
+            else high = mid-1;
+            
+        }
+        return (int)low-1;
     }
 }
