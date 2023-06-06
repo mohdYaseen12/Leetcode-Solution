9cbd1820ee69adc8bee3c2c7e9b92541964bbe4c
Md Yaseen <95025453+mohdYaseen12@users.noreply.github.com>
2023-06-06 14:24:17 +0530

    Time: 2 ms (99.71%), Space: 43 MB (33.72%) - LeetHub

--- 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
+++ 1171-remove-zero-sum-consecutive-nodes-from-linked-list/1171-remove-zero-sum-consecutive-nodes-from-linked-list.java
@@ -10,24 +10,50 @@
  */
 class Solution {
     public ListNode removeZeroSumSublists(ListNode head) {
-        Map<Integer,ListNode> map = new HashMap<>();
-        int sum = 0;
+//         Map<Integer,ListNode> map = new HashMap<>();
+//         int sum = 0;
         
-        for(ListNode curr = head; curr != null; curr = curr.next){
-            sum += curr.val;
-            map.put(sum,curr);
-        }
+//         for(ListNode curr = head; curr != null; curr = curr.next){
+//             sum += curr.val;
+//             map.put(sum,curr);
+//         }
         
-        sum = 0;
+//         sum = 0;
         
-        for(ListNode curr = head; curr!= null; curr = curr.next){
-            sum += curr.val;
-            ListNode node = map.get(sum);
+//         for(ListNode curr = head; curr!= null; curr = curr.next){
+//             sum += curr.val;
+//             ListNode node = map.get(sum);
             
-            if(sum == 0) head = node.next;
+//             if(sum == 0) head = node.next;
             
-            if(node != curr) curr.next = node.next;
+//             if(node != curr) curr.next = node.next;
+//         }
+//         return head;
+        
+        
+        
+        ListNode dummy= new ListNode(0);
+        dummy.next = head;
+        ListNode curr= dummy;
+        
+        while(curr != null){
+            int sum=0;
+            ListNode temp=curr.next;
+
+                while(temp != null){
+                    sum += temp.val;
+                    if(sum == 0){
+                        curr.next=temp.next;
+                        break;
+                    }
+                    temp=temp.next;
+                }
+
+                if(temp == null){
+                    curr=curr.next;
+                }
+
         }
-        return head;
+        return dummy.next;
     }
 }
