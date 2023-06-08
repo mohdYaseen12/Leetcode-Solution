524fe9f0350755a8eeaf6775f653356e0eb0f823
Md Yaseen <95025453+mohdYaseen12@users.noreply.github.com>
2023-06-08 14:02:02 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (39.65%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,32 +1,15 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Character> st = new Stack<>();
+        Stack<Integer> st = new Stack<>();
         int score = 0;
-        
         for(char ch : s.toCharArray()){
-            if(ch =='(') st.push(ch);
-            else{
-                // if peek is '(' -> means wee have found the corresponding pair
-                if(!st.isEmpty() && st.peek() == '('){
-                    st.pop();
-                    st.push('1');
-                }
-                else{ // if peek element is number
-                    int sum = 0;
-                    while(!st.isEmpty() && st.peek() != '('){
-                        sum += st.pop() - '0';
-                    }
-                    
-                    // pop() the '('
-                    st.pop();
-                    st.push((char)(2 * sum + '0'));
-                    
-                }
+            if(ch == '('){
+                st.push(score);
+                score = 0;
             }
-        }
-        
-        while(!st.isEmpty()){
-            score += st.pop() - '0';
+            else{
+                score = st.pop() + Math.max(2 * score, 1);
+            }
         }
         return score;
     }
