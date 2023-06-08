9cfdbae7a87b01d7269b8876f0c002c77f618ac4
Md Yaseen <95025453+mohdYaseen12@users.noreply.github.com>
2023-06-08 14:05:31 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (39.65%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.java
+++ 0856-score-of-parentheses/0856-score-of-parentheses.java
@@ -1,16 +1,38 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> st = new Stack<>();
+        // Stack<Integer> st = new Stack<>();
+        // int score = 0;
+        // for(char ch : s.toCharArray()){
+        //     if(ch == '('){
+        //         st.push(score);
+        //         score = 0;
+        //     }
+        //     else{
+        //         score = st.pop() + Math.max(2 * score, 1);
+        //     }
+        // }
+        // return score;
+        
+        
+        
+        // depth version
+        // constant space complexity
+        
+        int depth = 0;
         int score = 0;
-        for(char ch : s.toCharArray()){
-            if(ch == '('){
-                st.push(score);
-                score = 0;
-            }
+        int n = s.length();
+        
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch == '(') depth++;
             else{
-                score = st.pop() + Math.max(2 * score, 1);
+                depth--;
+                if(s.charAt(i-1) == '('){
+                    score += Math.pow(2,depth);
+                }
             }
         }
+        
         return score;
     }
 }
