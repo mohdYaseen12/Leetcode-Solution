8a6ca7bf2587d5bac7c439e2f731e908bb27ac30
Md Yaseen <95025453+mohdYaseen12@users.noreply.github.com>
2026-08-05 15:24:21 +0530

    Time: 1 ms (100%), Space: 46.5 MB (91.19%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,19 +1,44 @@
 class Solution {
+    // sorting method O(nlogn)
+    // public List<Integer> findMissingElements(int[] nums) {
+
+    //     List<Integer> ans = new ArrayList<>();
+    //     Arrays.sort(nums);
+        
+    //     int curr = nums[0];
+
+    //     for(int i =0; i<nums.length; i++) {
+    //         if(curr < nums[i]){
+    //             ans.add(curr);
+    //             i--;
+    //         }
+    //         curr++;
+    //     }
+
+    //     return ans;
+    // }
+
+
+    // boolean method O(n)
     public List<Integer> findMissingElements(int[] nums) {
 
         List<Integer> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        
-        int curr = nums[0];
+        boolean[] contains = new boolean[101];
 
-        for(int i =0; i<nums.length; i++) {
-            if(curr < nums[i]){
-                ans.add(curr);
-                i--;
-            }
-            curr++;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int num : nums) {
+            min = Math.min(min, num);
+            max = Math.max(max, num);
+            contains[num] = true;
+        }
+
+        for(int i = min; i<=max; i++){
+            if(!contains[i]) ans.add(i);
         }
 
         return ans;
+       
     }
 }
